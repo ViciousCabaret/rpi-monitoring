0aa986f7d0d4a2adaf89e5a85dea5b4017c0152d
mporebski <mikolaj.porebski@mindgram.com>
2024-04-26 03:08:16 +0200

    #master debug

--- test_stream_udp.py
+++ test_stream_udp.py
@@ -7,7 +7,8 @@ from picamera2.outputs import FfmpegOutput
 picam2 = Picamera2()
 picam2.configure(picam2.create_video_configuration(main={"size": (1280, 720),
                                                          "format": "YUV420"}))
-picam2.start_recording(H264Encoder(), output=FfmpegOutput("-f rtp udp://192.168.1.9:9000"))
+picam2.start_recording(H264Encoder(), output=FfmpegOutput("-f mpegts udp://192.168.1.9:9000"))
+# FfmpegOutput("-f mpegts udp://<ip-addr>:<port>")
 count = 0
 while True:
     print(f'hello [{count}]')
